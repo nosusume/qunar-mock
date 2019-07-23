1d6b709ff8c11af99895ca852184876f46100c6a
buckwheat <neolidy@foxmail.com>
2019-07-23 20:52:04 +0800

    template

--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+//多端样式统一
+import './asserts/reset.css'
+//1px问题
+import './assets/border.css'
+//移动端300ms延迟
+import fastClick from 'fastclick'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body);
 
 /* eslint-disable no-new */
 new Vue({
