2154ad5e7d29ac1ea17109ee6c9baf99e3d6d5f0
buckwheat <neolidy@foxmail.com>
2019-07-28 16:27:57 +0800

    city-ajax

--- src/components/city/city.vue
+++ src/components/city/city.vue
@@ -2,12 +2,13 @@
   <div>
     <city-header></city-header>
     <city-serach></city-serach>
-    <city-list></city-list>
-    <city-alphabet></city-alphabet>
+    <city-list :cities="cities" :hot="hotCities"></city-list>
+    <city-alphabet :cities="cities"></city-alphabet>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 import CityHeader from './cityHeader'
 import CitySerach from './serach'
 import CityList from './list'
@@ -19,6 +20,28 @@ export default {
     CitySerach,
     CityList,
     CityAlphabet
+  },
+  data () {
+    return {
+      cities: {},
+      hotCities: []
+    }
+  },
+  methods: {
+    getCityInfo () {
+      axios.get('/api/city.json').then(this.getCityInfoSucc)
+    },
+    getCityInfoSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.cities = data.cities
+        this.hotCities = data.hotCities
+      }
+    }
+  },
+  mounted () {
+    this.getCityInfo()
   }
 }
 </script>
