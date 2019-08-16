26d93c630fe19dca1aa81b044f2a9aac7e0d8273
buckwheat <neolidy@foxmail.com>
2019-08-16 21:30:55 +0800

    gallary

---
+++ src/common/gallary/gallary.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="container" @click="handleGallaryClick">
+    <div class="wrapper">
+      <swiper :options="swiperOption">
+        <swiper-slide v-for="(item, index) in imgs" :key="index">
+          <img class="gallary-img" :src="item" />
+        </swiper-slide>
+        <div class="gallary-pagination" slot="pagination"></div>
+      </swiper>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CommonGallary',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.gallary-pagination',
+        paginationType: 'fraction',
+        observeParents: true,
+        observer: true
+      }
+    }
+  },
+  methods: {
+    handleGallaryClick () {
+      this.$emit('close')
+    }
+  },
+  props: {
+    imgs: {
+      type: Array,
+      default () {
+        return [
+          'http://img1.qunarzz.com/sight/p0/1508/89/895a1b7add84f23faca053ce9e3153db.water.jpg_r_800x800_087730c7.jpg',
+          'http://img1.qunarzz.com/sight/p0/1906/e7/e7276de002459b50a3.img.jpg_r_800x800_5cdd4127.jpg'
+        ]
+      }
+    }
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+.container >>> .swiper-container {
+  overflow: inherit;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: fixed;
+  z-index: 99;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  background: black;
+
+  .wrapper {
+    height: 0;
+    width: 100%;
+    padding-bottom: 100%;
+
+    .gallary-img {
+      width: 100%;
+    }
+
+    .gallary-pagination {
+      position absolute
+      text-align: center;
+      color: #fff;
+      bottom -1rem
+    }
+  }
+}
+</style>
---
+++ src/components/detail/banner.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img  class="banner-image" src="http://img1.qunarzz.com/sight/p0/1508/89/895a1b7add84f23faca053ce9e3153db.water.jpg_600x330_93d96bdf.jpg" alt="banner-image">
+      <div class="banner-info">
+        <div class="banner-title">北京欢乐谷(AAAA景区)</div>
+        <div class="banner-number"><span class="iconfont banner-icon">&#xe63c;</span>25</div>
+      </div>
+    </div>
+    <common-gallary v-show="showGallary" @close="handleCloseEvent"></common-gallary>
+  </div>
+</template>
+
+<script>
+import CommonGallary from 'common/gallary/gallary'
+export default {
+  name: 'DetailBanner',
+  components: {
+    CommonGallary
+  },
+  data () {
+    return {
+      showGallary: false
+    }
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallary = true
+    },
+    handleCloseEvent () {
+      this.showGallary = false
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    position relative
+    overflow hidden
+    height  0
+    padding-bottom 55%
+    .banner-image
+      width 100%
+    .banner-info
+      display flex
+      position absolute
+      left 0
+      right 0
+      bottom 0
+      line-height .6rem
+      color #ffffff
+      background-image linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
+      .banner-title
+        flex 1
+        font-size .32rem
+        padding 0 .2rem
+      .banner-number
+        position absolute
+        left .1rem
+        bottom .6rem
+        line-height .4rem
+        border-radius .2rem
+        background rgba(0,0,0,0.8)
+        font-size .24rem
+        width 1.24rem
+        text-align center
+      .banner-icon
+        font-size .24rem
+</style>
