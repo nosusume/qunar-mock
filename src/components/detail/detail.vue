36a25f04a1d602daf1c2ee76ea9713546440b449
buckwheat <neolidy@foxmail.com>
2019-08-25 17:53:36 +0800

    detail-list

--- src/components/detail/detail.vue
+++ src/components/detail/detail.vue
@@ -2,6 +2,7 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
+    <detail-list :list="list"></detail-list>
     <div class="content"></div>
   </div>
 </template>
@@ -9,11 +10,36 @@
 <script>
 import DetailBanner from './banner'
 import DetailHeader from './header'
+import DetailList from './list'
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      list: [
+        {
+          title: '成人票',
+          children: [
+            {title: '成人五馆联票'},
+            {title: '成人三馆联票',
+              children: [{
+                title: '成人五馆联票-不卖'
+              }]
+            }
+          ]
+        },
+        {
+          title: '学生票'
+        },
+        {
+          title: '特惠票'
+        }
+      ]
+    }
   }
 }
 </script>
